1a4e6f83f5d5bb9beb81d35665f9e12196ab72b9
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-21 13:35:44 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,25 @@
-fx_version 'cerulean'
-game 'gta5'
+--[[ FX Information ]]--
+fx_version   'cerulean'
+use_fxv2_oal 'yes'
+lua54        'yes'
+game         'gta5'
 
-name "NoPixel Inspired Dongle Ped"
-author "Made with love by Samuel#0008"
-version "1.0"
+--[[ Resource Information ]]--
+name         'sd-dongleped'
+author       'Samuel#0008 | Judd#7644'
+version      '1.0.2'
+repository   'https://github.com/juddisjudd/sd-dongleped'
+description  'NoPixel Inspired Dongle Ped'
 
 client_scripts {
 	'client.lua',
-	'config.lua'
 }
 
 server_scripts {
 	'server.lua',
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
 	'config.lua'
 }
