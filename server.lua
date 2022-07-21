16aa347fb6a3956e10bba8b6faf642881e2bd4cc
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-21 13:55:54 -0700

    not finished, def broken

--- server.lua
+++ server.lua
@@ -1,16 +1,12 @@
-local QBCore = exports["qb-core"]:GetCoreObject()
-
 RegisterNetEvent('sd-dongle:server:buyshit', function(ped)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not src or not Player or not ped then return end
-    local cash = Player.PlayerData.money[Config.Shop[ped].type]
+    local player = Ox.GetPlayer(source)
+    if not source or not player or not ped then return end
+    local cash = player.PlayerData.money[Config.Shop[ped].type]
 
     if cash >= Config.Shop[ped].price then
-        Player.Functions.RemoveMoney(Config.Shop[ped].type, Config.Shop[ped].price)
-        Player.Functions.AddItem(Config.Shop[ped].item, 1)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.Shop[ped].item], "add")
+        exports.ox_inventory:RemoveItem(source, 'money', 100)
+        exports.ox_inventory:AddItem(source, Config.Shop[ped].item, 1)
     else
-        TriggerClientEvent('QBCore:Notify', src, 'Not enough '..Config.Shop[ped].type, 'error')
+        TriggerClientEvent('ox_lib:notify', source, 'Not enough '..Config.Shop[ped].type, 'error')
     end
 end)
