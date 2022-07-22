8b494254fa8df1f08920a385d1e88406d40ab680
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-21 23:11:34 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,13 @@
-RegisterNetEvent('sd-dongle:server:buyshit', function(source)
+RegisterNetEvent('sd-dongle:server:buyshit', function(ped)
     local player = Ox.GetPlayer(source)
-    if not source or not player then return end
+    if not source or not Player or not ped then return end
     local cash = exports.ox_inventory:GetItem(source, 'money', returnsCount)
 
-    if (exports.ox_inventory:GetItem(cash).count < Config.Shop[source].price) then
-        exports.ox_inventory:RemoveItem(source, 'money', Config.Shop[source].price)
-        exports.ox_inventory:AddItem(source, Config.Shop[source].item, 1)
-        TriggerClientEvent('ox_lib:notify', source, {title = 'Purchased '..Config.Shop[source].label, type = 'success'})
+    if (exports.ox_inventory:GetItem(cash).count < Config.Shop[ped].price) then
+        exports.ox_inventory:RemoveItem(source, 'money', Config.Shop[ped].price)
+        exports.ox_inventory:AddItem(source, Config.Shop[ped].item, 1)
+        TriggerClientEvent('ox_lib:notify', source, {title = 'Purchased '..Config.Shop[ped].label, type = 'success'})
     else
-        TriggerClientEvent('ox_lib:notify', source, {title = 'Not enough '..Config.Shop[source].type, type = 'error'})
+        TriggerClientEvent('ox_lib:notify', source, {title = 'Not enough '..Config.Shop[ped].type, type = 'error'})
     end
 end)
-
