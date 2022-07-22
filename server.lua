676746376b5ce13a03e20e3685575dbb8934d256
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-22 00:13:30 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,13 +1,12 @@
 local ox_inventory = exports.ox_inventory
-local count = 0
 
 RegisterNetEvent('sd-dongle:server:buyshit', function(ped)
     local player = Ox.GetPlayer(source)
     if not source or not Player or not ped then return end
-    local cash = ox_inventory:GetItem(source, 'money', returnsCount)
+    local cash = ox_inventory:Search(source, 'count', 'money')
     print(cash)
 
-    if (ox_inventory:GetItem(cash).count < Config.Shop[ped].price) then
+    if (cash >= Config.Shop[ped].price) then
         ox_inventory:RemoveItem(source, 'money', Config.Shop[ped].price)
         ox_inventory:AddItem(source, Config.Shop[ped].item, 1)
         TriggerClientEvent('ox_lib:notify', source, {title = 'Purchased '..Config.Shop[ped].label, type = 'success'})
